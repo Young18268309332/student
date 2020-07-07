5292adfbd48c5abe711cce38868f8929d25e1035
Young18268309332 <1160576782@qq.com>
2020-07-07 14:57:00 +0800

    第一次上传

---
+++ dior/day01.py
@@ -0,0 +1,792 @@
+# a=100
+# b=200
+#
+# print (a)
+# print(a+b)
+#
+# a = [1,2,3,4,5,6,7,]
+# a[2]
+#
+# a = 1,2,3,4,5,5,6
+# a,b,c,d = [12,12,44,55]
+# print(a,b,c,d)
+#
+# a = {"name":"tom","age":"22","city":"bingjing"}
+# print(a["city"])
+#
+#
+# blibli = {123,123,456,456}
+# print(blibli)
+#
+#
+#
+# w, q, *z =(44,66,88,454,78,353,546)
+# print(w)
+# print(q)
+# print(z)
+#
+#
+# g,y,*a = (232,564,86,867,565,64)
+# print(g)
+# print(y)
+# print(a)
+#
+# a =231
+# b =345
+# c+=b
+# print(c)
+#
+# a=1
+# b=2
+# print(a==b)
+# print(a<b)
+# print(a>b)
+#
+# a=1
+# b=2
+# c=3
+# d+=b + c
+# print(d)
+#
+# a=134325435
+# print(a % 10)
+# print(a % 100)
+# print(a % 1000)
+#
+# a=412435
+# print(a%10)
+#
+# b=a//10
+# print(b%10)
+#
+#
+# a=10
+# b=20
+# a-=b
+# print(a)
+#
+# a=10
+# b=20
+# a*=b
+# print(a)
+#
+# a=10
+# b=20
+# a/=b
+# print(a)
+#
+# a=10
+# b=20
+# a**=b
+# print(a)
+#
+# a=10
+# b=20
+# a//=b
+# print(a)
+#
+# a=10
+# b=20
+# c=30
+# d=40
+# a+=b+c+d
+# print(a)
+#
+# m = ("lv","dior","mk","chanel")
+# #if("lv" in m )
+#    # print("ok")
+#
+#    # print("out")
+#
+#
+# score = 50
+#
+# if(score>0 and score<60):
+#
+#    print("不及格",)
+#
+# if(score>60 and score<70):
+#
+#    print("及格")
+#
+# if(score>70 and score<80):
+#
+#    print("良好")
+#
+#
+# score3 = [44,56,77,78,89,96,94,109]
+#
+# for score in score3:
+#
+#     if(score>0 and score<60):
+#       print("不及格",)
+#
+#     elif(score>60 and score<70):
+#       print("及格")
+#
+#     elif(score>70 and score<80):
+#       print("良好")
+#
+#     elif(score>80 and score<=100):
+#       print("优秀")
+#
+#     else:
+#       print("请输入正确的成绩")
+#
+#
+# score5 =  [78,24,56,73,92,24,54,34]
+# for score in score5:
+#     if (score > 0 and score < 60):
+#         print("不及格", )
+#
+#     elif (score > 60 and score < 70):
+#         print("及格")
+#
+#     elif (score > 70 and score < 80):
+#         print("良好")
+#
+#     elif (score > 80 and score <= 100):
+#         print("优秀")
+#
+#     else:
+#         print("请输入正确的成绩")
+#
+#
+# s = 1
+# for a in range(10,0,-1):
+#  s = s * a
+# print(s)
+#
+#
+#
+# flag = True
+# a = 89
+# while flag:
+#     b = int(input("请输入数字"))
+#     if b > a:
+#         print("大了")
+#     elif b < a:
+#         print("小了")
+#     else :
+#         print("对了")
+#         flag = False
+#
+#
+#
+#
+# flag = True
+# a = 88
+# while flag:
+#     b = int (input("请输入数字"))
+#     if b > a:
+#         print("big")
+#     elif b < a:
+#         print("small")
+#     else:
+#         print("yes")
+#         flag = False
+#
+#
+
+# s = 1
+# for i in range(100,0,-1):
+#   s =s+i
+# print(s)
+
+
+#100u以内乘
+# s = 1
+# for e in range(100,0,-1):
+#     s += e
+# print(s)
+#
+# score = 50
+# if (score >0 and score <60):
+#     print("不及格")
+# elif (score >= 60 and score <=70):
+#     print("及格")
+# elif (score > 70 and score <=80 ):
+#     print("良好")
+# elif (score > 80 and score <=100):
+#     print("优秀")
+# else:
+#     print("请输入正确成绩")
+#
+# score1 = [20,43,65,86,95,34]
+# for score in score1:
+#     if (score >0 and score <60):
+#         print("不及格")
+#     elif (score >= 60 and score <=70):
+#         print("及格")
+#     elif (score > 70 and score <=80 ):
+#         print("良好")
+#     elif (score > 80 and score <=100):
+#         print("优秀")
+#     else:
+#         print("请输入正确成绩")
+
+
+#
+# flag = True
+# a = 94
+# for b in range(0,100):
+# #while flag:
+#    # b = int(input("请输入数字"))
+#     if b>a:
+#         print("big")
+#     elif b<a:
+#         print("small")
+#     else:
+#         print("right")
+#         flag = False
+#         break;
+
+
+
+# a=131435467
+# print(a%10)
+#
+# a=131435467
+#
+# print(a%10);
+#
+#
+# a //=10
+# print(a)
+# print(a%10);
+#
+
+
+# a=3467358
+# print(a%10)
+# a//=10
+# print(a%10)
+#
+# score1 = [12,56,78,97,35,57,44]
+# for score in score1:
+#     if(score >0 and score<60):
+#         print("不及格")
+#     elif(score >=60 and score <70):
+#         print("及格")
+#     elif(score >70 and score <80):
+#         print("良好")
+#     elif(score >80 and score <=100):
+#         print ("优秀")
+#     else:
+#         print ("请输入正确的成绩")
+#
+
+# flag = True
+# s = 20
+# while flag:
+#     b = int(input("请输入数字"))                   #数字随机游戏
+#     if b>s :
+#         print("big")
+#     elif b<s :
+#         print("small")
+#     else:
+#         print("right")
+#         flag = False
+
+
+#
+# a,t,y,*z=[13,67,88,34,23,99,463,26,41,454]
+# print(a)
+# print(t)
+# print(y)
+# print(*z)
+
+
+
+# for i in range (1,100):
+#     if (i % 2 == 0):
+#
+#         print (i)
+
+#
+# for y in range (1,100):
+#     if (y % 4 != 0):   #求能被四整除的1-100之间的数字
+#         continue
+#     print (y)
+
+
+# 定义一个两个数商和余数的方法
+
+
+# def m (a,b):
+#     print(a % b)
+#     print(a // b)
+# m(500, 108)
+# m(10, 98)
+
+# def m (a,b):
+#     if (b==0):
+#         return None               #定义变量
+#     else :
+#         return(a % b,a // b)
+# op = m (20,30)                          #取变量
+# if op is None:
+#     print ("参数错误")                  用变量
+# else :
+#     print ("商：",op[0],"余:",op[1])
+
+
+
+# def r (a ,b):
+#     print ("商为：",a % b,"余为：",a //b)             #定义一个两个数商和余数的方法
+# r(30,60)
+
+# def p (a,b):
+#     if (b==0):
+#         return None
+#     else:
+#         return(a%b,a//b)
+#
+# w = p(b=20,a=88)
+#     if w is None:
+#         print("参数错误")
+#
+#     else :
+#         print ("商：",p[0],"余:",p[[1])
+#
+
+
+
+
+# def bin (c,b=20):
+#     return c+b
+# print(bin(20))
+
+# def yo(*args,**kwargs):    #*args接收动态位置参数，**kwargs 接收动态关键参数
+#     s=0
+#     for t in(args):            #不知道有多少参数，求传的参数的和
+#         s+=t
+#     return s
+# print(yo(124,654,98679,7989))
+
+
+
+#求100以内的能被3 整除的数字
+#
+# for y in range (1,100):
+#     if (y % 3 !=0):
+#         continue
+#     print(y)
+
+
+    #求两个数的商和余数
+# def hi (k,j):
+#     if (j==0):
+#         return None
+#     else:
+#         print (k % j,k // j)
+#
+# fg = hi(20 ,30)
+# if fg is None:
+#     print("参数错误")
+# else:
+#     print("商为，",fg[0],"余为,",fg[1])
+
+
+# #不知道有多少参数，求传的参数的和
+# def rt (name,*args,**kwargs):
+#     print(kwargs)
+#     # print(name)
+#     k = 0
+#     for rt in(args):
+#         k+=rt
+#     return k
+# print(rt(23,56,76,89,9,54,))
+# print(rt(name="小明"))
+
+
+#
+# def rt (*args):
+#
+#     k = 0
+#     for rt in(args):
+#         k+=rt
+#     return k
+# print(rt(23,56,76,89,9,54,))
+#
+
+#
+
+  # class 用途
+# class option():#定义类的名
+#     a=None
+#     b=None
+#     res=None
+
+#     def input(self,a,b):   #创建类，并调用
+#         self.a=a
+#         self.b=b
+#     def sum(self):
+#         self.res = self.a + self.b
+#     def div(self):
+#         self.res = self.a / self.b
+#     def get_result(self):
+#         print(self.res)
+#
+#
+# c = option()  #实例调用类
+# c.input(50,80)
+# c.sum()
+# c.get_result()
+# c.div()
+# c.get_result()
+
+
+
+#
+# class option():
+#     res=None
+#
+#     def __init__(self,a,b):   #魔法函数，类实例化的时候调用，又称构造方法
+#         self.a=a
+#         self.b=b
+#     def sum(self):
+#         self.res = self.a + self.b
+#     def div(self):
+#         self.res = self.a / self.b
+#     def get_result(self):
+#         print(self.res)
+#
+#
+#
+# c = option(98,45)
+# c.sum()
+# c.get_result()
+#
+
+#九九乘法表
+# for k in range (1,10):
+#     for h in range (1,k+1):
+#         print(h ,"X", k,"=",h*k, end="\t")
+#     print()
+
+#
+# for k in range (1,10):
+#     print(k)
+
+#
+#
+# # 冒泡排序
+# l=[53,5,2,6,45,76,2,978,98,43,63]
+#
+# length = len(l)
+# for h in range (length-1,0,-1):                 #外层循环确定排好序的数据下标
+#     for g in range (h):                           #遍历未排好的列表
+#         if (l[g] > l[g+1]):                         #判断相邻两个数据，前边的如果大于后边的，则交换两个数据的位置
+#             l[g],l[g+1] = l[g+1],l[g]
+# print(l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# w=[24,54,678,5,5,324,45,756,423,34]
+# length = len(w)
+# for k in range(length-1,0,-1):
+#     for h in range(k):
+#         if w[h]>w[h+1]:
+#             w[h],w[h+1] = w[h+1],w[h]
+# print(w)
+
+
+
+
+
+
+
+#
+# #九九乘法表
+# for h in range(1,10):
+#     for l in range(1,h+1):
+#         print (h,"X",l,"=",h*l,end="\t")
+#     print()
+#
+#     #你好循环10遍
+
+# for b in range :
+#     print(a)
+
+
+
+#继承
+
+#
+
+# class Parent():
+#     money = 1000000000000
+#     car=50
+#     train=20
+#     house=100
+#     __clothes=800
+#     def __init__(self,a):
+#         self.a=a
+#     def inherit(self):
+#         print("点石成金")
+#
+#
+# class Child(Parent):
+#     aihao = 'shopping'
+#
+#     pass
+#
+#
+#
+# c=Child()
+# def __init__()
+# print(c.aihao)
+# print(c.house)
+# print(c.money)
+
+
+
+#封装
+#
+#
+# class bbb():
+#     pub_res = "公有变量"
+#     __pri_res = "私有变量1"   #双下划线变成私有，别人不能调用
+#     _pri_res = "私有变量2"
+#
+#
+#     def pub_function(self):
+#         print("公有方法")
+#     def _pri_function(self):
+#         print("私有方法1")
+#     def __pri_function(self):
+#         print("私有方法2")
+#
+#
+#
+# print(bbb.pub_res)
+# print(bbb._pri_res)
+# print(bbb().pub_function())
+
+
+
+
+# from dior.day02 import day01
+# # a="我是变量1的变量"
+# # def name():
+# #     print("我是变量1的变量")
+# # class Test():
+# #     print("我是模块1中的类")
+
+# import random
+# player = input('剪刀','石头','布')
+# computer = random.randint(0,2)
+# if (player=='剪刀' and computer==1 or player=='石头' and computer==2 or player=='布' and computer==0)
+#     print('computer win')
+# elif()
+
+
+# import requests
+# a=244
+# b=1,3,5,6
+# print(list(b))
+# print(tuple(b))
+# print(str(a))
+# print(set(b))
+
+
+
+# '''
+# s=""sfdfghfrtywthteh
+# ferwtwrgths
+# gsdgrysrtywr
+# gertrhgydhgjuy
+#
+#
+# '''
+
+
+
+# f=open('young.txt','w')
+# f.write('python\n,faerrgr\n,sghxbgvhtryh\n,qtertgutey')
+# f .close()
+
+
+
+# f=open('young.txt','r')
+# print (f.read())
+# print(f.readable())
+# print(f.readline())
+# print(f.readlines())
+# f.close()
+
+
+#
+# a="1234567890"
+# print(a[3])
+# print(a[-1:0 :-1])
+# print( a[-1: : -1])
+# print(a[: : 1])
+# print(a[3: -2 : -1])
+
+#
+# l="gretgfarg,hydgjn,aetgreth"
+# print(l.split(","))
+# with open("young.txt",'r') as f:    #with 使用一个上下文管理器
+#     lines=f.readlines
+#     print(lines)
+#     for i in lines:
+#         print(i.replace("\n",""),end="")   #print默认带一个换行
+#
+
+
+#
+# for h in range(1,10):
+#     for l in range(1,h+1):
+#         print ("{} X {} = {}".format(h,l,h*l),end="\t")
+#     print()
+#
+
+
+# for h in range(1,10):
+#     for l in range(1,h+1):
+#         print ("%d X %d = %d"%(h,l,h*l),end="\t")
+#     print()
+#
+# o=[13,64,6,54,5,3,7,8,3]
+# o[1]=100
+# print(o)
+# o[3: 4]=1000,100000
+# print(o)
+# o[0:1]=0.1,0.2
+# print(o)
+
+
+
+# print("{:0>10}".format(123))  #左对齐
+# print("{: <10}".format(123))   #右对齐
+# print("{: 0=10}".format(123))    #居中
+# print("{: .1f}".format(2.45468786))  #取小数的位数
+
+
+#新增数据
+# p=[1,4,6,4,3,6]# p.append("student")#一次添加一条数据
+# p.extend({'123',123,5345}) #一次添加多个数据
+# p.insert(1,"你是谁") #新增指定位置的数据
+
+# print(p.pop()) #默认删除最后一位
+# print(p.pop(1)) #下标删除
+# p.remove(6) #指定数据删除，一次只能删一次
+# p=[True,23,4,6,7,6]
+# p.remove(2) #python True=1 false=0
+# print(p)
+
+# p.clear()  #清除整个列表
+# print()
+
+#字典取值
+# n={"name":"Tom","age":"20","sex":"men"}
+# for key in n:
+#     print(n[key])  用列表取key的值
+
+#
+# for k,v in n.items(): #用items取key的值
+#     print(k,v)
+
+
+
+# n={"name":"Tom","age":"20","sex":"men"}   #添加数据
+# n.update({"addr":"上海闵行","学历":"本科"})
+# print(n)
+
+
+
+#字典中要过滤旧数据，形成新的数据：
+# n={"name":"Tom","age":"20","sex":"men"}
+# s={}
+# for key in n:
+#     if key == "addr":
+#         continue
+#     s[key] = n[key]
+# print(s)
+
+
+
+
+#异常处理try....except用法
+# def div(b,c):
+#     try:
+#         return b/c
+#     except ZeroDivisionError as e:   #可以根据要报错的内容except ,前提是要知道报错的内容 加as 可以把报错内容写入文件
+#     print(e)  #可写入文件
+#         return
+# print(div(10,0))
+# try:
+#     f = open("yyyyy.txt","r")   #打开不存在的文件名会报错，用try...except 捕捉异常
+#     print(f.read())
+#     f.close()
+# except :
+#     print("文件不存在")
+# print("操作完文件")
+
+# try...except 的另一种用法
+# def div(a,b):
+#     try:
+#         print(a/b)
+#     except ZeroDivisionError as e:
+#         print(e)
+#     else:
+#         print("除法执行成功")
+#     finally:
+#         print("不管try中是否报错，都会被执行")
+#
+# div(1,0)
+
+
+# 用户自定义异常类型，只要该类继承了Exception类即可，至于类的主题内容用户自定义，可参考官方异常类
+class CustomException(Exception):
+    def __init__(self,value="值不能为0"):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+a=0
+try:
+    if a == 0:
+        print("a={}触发异常".format(a))
+        raise CustomException
+    print("a={}未触发异常".format(a))
+except CustomException as e:
+    print(e)
+
+
---
+++ dior/day02.py
@@ -0,0 +1,131 @@
+
+
+#求100以内的能被3 整除的数字
+#
+# for y in range (1,100):
+#     if (y % 3 !=0):
+#         continue
+#     print(y)
+
+
+
+
+
+# # 求两数字的商和余
+# def j (b,f):
+#     if (f==0):
+#         print ("error")
+#     else:
+#         print(b % f,b // f)
+#
+# y = j(20,90)
+# if y is None:
+#     print ("error")
+# else :
+#     print("商为",[0],"余为",[1])
+#
+
+
+#九九乘法表
+# for j in range (1,10):
+#     for k in range (1,j+1):
+#         print(j,"X",k,"=",j*k,end = "\t")
+#     print()
+
+
+#冒泡排序
+# l=[31,545,76,87,8,54,2557,417,876,74,54]
+# length = len(l)
+# for k in range (length-1,0,-1):
+#     for g in range (k): #本来是k+1，但是右边要交换两位元素
+#         if l[g] > l[g+1]:
+#             l[g],l[g+1] = l[g+1],l[g]
+# print(l)
+
+
+#不知道有多少参数，求传的参数的和
+def p (*args):
+    k=0
+    for s in (args):
+        k+=s
+    return k
+print(p(12,78,5,45))
+
+
+
+
+
+  # class 用途
+# class option():#定义类的名
+#     a=None
+#     b=None
+#     res=None
+
+#     def input(self,a,b):   #创建类，并调用
+#         self.a=a
+#         self.b=b
+#     def sum(self):
+#         self.res = self.a + self.b
+#     def div(self):
+#         self.res = self.a / self.b
+#     def get_result(self):
+#         print(self.res)
+#
+#
+# c = option()  #实例调用类
+# c.input(50,80)
+# c.sum()
+# c.get_result()
+# c.div()
+# c.get_result()
+
+
+
+
+
+#
+# class option():
+#     res=None
+#
+#     def __init__(self,a,b):   #魔法函数，类实例化的时候调用，又称构造方法
+#         self.a=a
+#         self.b=b
+#     def sum(self):
+#         self.res = self.a + self.b
+#     def div(self):
+#         self.res = self.a / self.b
+#     def get_result(self):
+#         print(self.res)
+#
+#
+#
+# c = option(98,45)
+# c.sum()
+# c.get_result()
+#
+
+
+
+
+# 冒泡排序
+# l=[53,5,2,6,45,76,2,978,98,43,63]
+#
+# length = len(l)
+# for h in range (length-1,0,-1):                 #外层循环确定排好序的数据下标
+#     for g in range (h):                           #遍历未排好的列表
+#         if (l[g] > l[g+1]):                         #判断相邻两个数据，前边的如果大于后边的，则交换两个数据的位置
+#             l[g],l[g+1] = l[g+1],l[g]
+# print(l)
+#
+
+from dior.day01 import a,name,Test
+
+print(a)
+# a="我是变量2的变量"
+# def name():
+#     print("我是变量2的变量")
+# class test():
+#     print("我是模块2中的类")
+    # name = "我是模块2中的类"
+# print(2006A.day1,2)
+# print(Test.name)
